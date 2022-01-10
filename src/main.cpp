907c44fc99e5b7cedd403aee9b7e7e56c392a695
YonkoSalar <salaradel4@gmail.com>
2022-01-10 21:48:36 +0100

    Finished matrix library

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,14 @@
 #include <stdio.h> /* printf, scanf, puts, NULL */
 #include <random>
 #include <type_traits>
+#include <stdexcept>
 
 using namespace std;
+
+
+/////////////////
+// PERCEPTRON //
+////////////////
 
 class Perceptron
 {
@@ -65,7 +71,12 @@ public:
     }
 };
 
-// Simple matrix class for
+
+
+
+//////////////////////
+//      MATRIX     //
+/////////////////////
 class Matrix
 {
 private:
@@ -74,6 +85,7 @@ private:
 
 public:
     // Initlize matrix with zeros
+    
     Matrix(int rows, int cols) : rows_(rows), cols_(cols)
     {
         // Allocate space for matrix
@@ -93,17 +105,93 @@ public:
         }
     }
 
-    // use this function to scale our matrix
-    void multiply(float num)
+
+    void randomize()
     {
-        // Scalar
-        for (int i = 0; i < rows_; i++)
+         // Generate random number between [-1, 1]
+        mt19937 rng(std::random_device{}());
+        uniform_int_distribution<> dist(0, 10);
+
+
+        for(int i = 0; i < rows_; i++)
         {
-            for (int j = 0; j < cols_; j++)
+            for(int j = 0; j < cols_; j++)
             {
-                matrix_[i][j] = matrix_[i][j] * num;
+                matrix_[i][j] = dist(rng);
             }
         }
+
+
+    }
+
+
+
+    // use this function to scale our matrix
+     template <typename T>
+    T multiply(T num)
+    {
+
+        try 
+        {
+             // Matrix product
+            if constexpr (std::is_same<T, Matrix>::value)
+            {
+                // Chekc if matix product is possible
+                if(cols_ != num.rows_)
+                {
+                    throw runtime_error("Matrid product not possible!");
+                }
+                
+                // New Matrix
+                Matrix result = Matrix(rows_, num.cols_);
+
+                // Matrix product
+                for(int i = 0; i < result.rows_; i++)
+                {
+                    for(int j = 0; j < result.cols_; j++)
+                    {
+                        
+                        for(int k = 0; k < rows_; k++)
+                        {
+                            result.matrix_[i][j] += matrix_[i][k] * num.matrix_[k][j];
+                        }
+                    }
+                }
+
+                return result;
+
+
+
+
+            }
+            // Scalar
+            else if constexpr (std::is_same<T, float>::value)
+            {
+                
+                for (int i = 0; i < rows_; i++)
+                {
+                    for (int j = 0; j < cols_; j++)
+                    {
+                        matrix_[i][j] = matrix_[i][j] * num;
+                    }
+                }
+
+            }
+            else 
+            {
+                throw runtime_error("Undefined type");
+            }
+
+        }
+
+        catch (const std::exception &ex)
+        {
+            cout << ex.what() << "\n";
+        }
+
+       
+        
+        
     }
 
 
@@ -112,34 +200,64 @@ public:
     template <typename T>
     void add(T num)
     {
+        try {
        
-        //If type is a single number
-        if constexpr (std::is_same<T, float>::value)
-        {
-            cout << "inside if" << endl;
-            for (int i = 0; i < rows_; i++)
+            //If type is a single number
+            if constexpr (std::is_same<T, float>::value)
             {
-                for (int j = 0; j < cols_; j++)
+                cout << "inside if" << endl;
+                for (int i = 0; i < rows_; i++)
                 {
-                    matrix_[i][j] = matrix_[i][j] + num;
-                }
-            }
-        }
-
-         // If type is a matrix
-        else if constexpr (std::is_same<T, Matrix>::value)
-        {
-            cout << "hallo" << endl;
-            for (int i = 0; i < rows_; i++)
-            {
-                for (int j = 0; j < cols_; j++)
-                {
-                    matrix_[i][j] = matrix_[i][j] + num.matrix_[i][j];
+                    for (int j = 0; j < cols_; j++)
+                    {
+                        matrix_[i][j] = matrix_[i][j] + num;
+                    }
                 }
             }
 
+            // If type is a matrix
+            else if constexpr (std::is_same<T, Matrix>::value)
+            {
+                cout << "hallo" << endl;
+                for (int i = 0; i < rows_; i++)
+                {
+                    for (int j = 0; j < cols_; j++)
+                    {
+                        matrix_[i][j] = matrix_[i][j] + num.matrix_[i][j];
+                    }
+                }
+
+            }
+            else 
+            {
+                throw(num);
+            }
+
         }
+
+        catch(T num)
+        {
+            cout << "Type is undefined - Must be either matrix or float" << endl;
+            cout << "Type of variable num is: " << typeid(num).name() << endl;
+        }
+      
         
+    }
+
+    Matrix transpose()
+    {
+        Matrix result = Matrix(cols_, rows_);
+
+        for(int i = 0; i < rows_; i++)
+        {
+            for(int j = 0; j < cols_; j++)
+            {
+                result.matrix_[j][i] = matrix_[i][j];
+            }
+        }
+
+        return result;
+
     }
 
     // Print matrix
@@ -156,6 +274,43 @@ public:
     }
 };
 
+
+//////////////////////
+//  NEURAL NETWORK //
+/////////////////////
+
+
+class NeuralNetwork 
+{
+    private:
+        int input_nodes;
+        int hidden_nodes;
+        int output_nodes;
+
+
+
+    public: 
+        NeuralNetwork(int input_nodes, int hidden_nodes, int output_nodes) 
+        : input_nodes(input_nodes), hidden_nodes(hidden_nodes), output_nodes(output_nodes)
+        {
+
+        }
+
+
+        void feed_forward()
+        {
+
+        }
+
+        void backward_pass()
+        {
+
+        }
+
+};
+
+
+
 int main()
 {
 
@@ -168,18 +323,30 @@ int main()
 
     // std::cout << "hello world" << std::endl;
 
-    Matrix matrix1(2, 2);
+    Matrix matrix1(3,3);
 
+    matrix1.randomize();
+    matrix1.print();
+
+    /*
     Matrix matrix2(2, 2);
 
-    float num = 5.0;
+    
 
-    matrix1.add<float>(num);
-    matrix2.add<float>(num);
+    
+    matrix1.add<float>(5.0);
+    matrix2.add<float>(2.0);
 
-
-    matrix1.add<Matrix>(matrix2);
-
+    
 
     matrix1.print();
+    matrix2.print();
+
+
+    Matrix matrix3 = matrix1.multiply(matrix2);
+
+    matrix3.print();
+    */
+
+
 }
